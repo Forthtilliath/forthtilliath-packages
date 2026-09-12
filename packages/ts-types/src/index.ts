a75696793dd1f5601f0154edee6203a42cc45ed2
forthtilliath <vincent.lisita+gh@gmail.com>
2026-09-12 05:21:42 +0200

    chore: strip superfluous comments in ts-types

--- packages/ts-types/src/index.ts
+++ packages/ts-types/src/index.ts
@@ -1,6 +1,3 @@
 export type * from "./array/index.js";
 export type * from "./helpers/index.js";
 export type * from "./object/index.js";
-
-// https://github.com/piotrwitek/utility-types
-// https://millsp.github.io/ts-toolbelt/
