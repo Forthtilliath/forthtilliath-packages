10bf37ed1616efc3d1e758b33f75b494354dde36
forthtilliath <vincent.lisita+gh@gmail.com>
2026-09-12 20:49:32 +0200

    feat: add factory functions to angular and storybook eslint configs

--- packages/eslint-config/src/storybook.js
+++ packages/eslint-config/src/storybook.js
@@ -1,24 +1,35 @@
 import { defineConfig } from "eslint/config";
 import storybook from "eslint-plugin-storybook";
 
-import { reactConfig } from "./react.js";
+import { createReactConfig } from "./react.js";
 
 /**
- * A custom ESLint configuration for libraries that use React.
+ * A custom ESLint configuration for libraries that use React and Storybook.
+ *
+ * @param {import("./base.js").BaseConfigOptions} [options]
+ * @returns {import("eslint").Linter.Config[]} */
+export function createStorybookConfig(options) {
+  return defineConfig([
+    ...createReactConfig(options),
+    ...storybook.configs["flat/recommended"],
+    {
+      ignores: ["!.storybook"],
+    },
+    {
+      // Should match the `stories` property in .storybook/main.js|ts.
+      files: ["**/*.stories.@(ts|tsx|js|jsx|mjs|cjs)"],
+      rules: {
+        "storybook/csf-component": "error",
+        "storybook/default-exports": "off",
+      },
+    },
+  ]);
+}
+
+/**
+ * A custom ESLint configuration for libraries that use React and Storybook,
+ * with the default options. Use `createStorybookConfig(options)` instead to
+ * customize it.
  *
  * @type {import("eslint").Linter.Config[]} */
-export const storybookConfig = defineConfig([
-  ...reactConfig,
-  ...storybook.configs["flat/recommended"],
-  {
-    ignores: ["!.storybook"],
-  },
-  {
-    // Should match the `stories` property in .storybook/main.js|ts.
-    files: ["**/*.stories.@(ts|tsx|js|jsx|mjs|cjs)"],
-    rules: {
-      "storybook/csf-component": "error",
-      "storybook/default-exports": "off",
-    },
-  },
-]);
+export const storybookConfig = createStorybookConfig();
