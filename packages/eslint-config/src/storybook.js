0c35d56f58bdfb5f821d5b69d6a7d824d77c52fd
forthtilliath <vincent.lisita+gh@gmail.com>
2026-09-12 05:26:08 +0200

    chore: strip superfluous comments in eslint-config

--- packages/eslint-config/src/storybook.js
+++ packages/eslint-config/src/storybook.js
@@ -14,12 +14,10 @@ export const storybookConfig = defineConfig([
     ignores: ["!.storybook"],
   },
   {
-    // 👇 This should match the `stories` property in .storybook/main.js|ts
+    // Should match the `stories` property in .storybook/main.js|ts.
     files: ["**/*.stories.@(ts|tsx|js|jsx|mjs|cjs)"],
     rules: {
-      // 👇 Enable this rule
       "storybook/csf-component": "error",
-      // 👇 Disable this rule
       "storybook/default-exports": "off",
     },
   },
