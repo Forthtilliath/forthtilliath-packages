98b82e1ce5050f70e33570cf012f4316afdf2a85
forthtilliath <vincent.lisita+gh@gmail.com>
2026-09-14 13:49:50 +0200

    feat: add root barrel export to ts-kit

---
+++ packages/ts-kit/src/files/index.ts
@@ -0,0 +1,1 @@
+export * from "./downloadText.js";
---
+++ packages/ts-kit/src/index.ts
@@ -0,0 +1,15 @@
+// Barrel: re-exports every category so consumers can do a single
+//   import { chunk, pick, sleep, clamp } from "@forthtilliath/ts-kit";
+// instead of one import line per module. Subpath imports
+// (`@forthtilliath/ts-kit/array/chunk`) still work if you want to keep
+// bundles minimal, but this is the default, low-friction entry point.
+export * from "./array/index.js";
+export * from "./async/index.js";
+export * from "./classes/index.js";
+export * from "./date/index.js";
+export * from "./files/index.js";
+export * from "./id/index.js";
+export * from "./maths/index.js";
+export * from "./number/index.js";
+export * from "./object/index.js";
+export * from "./string/index.js";
