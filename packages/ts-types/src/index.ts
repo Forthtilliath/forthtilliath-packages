b8da30c7d711aa70445da8093338c1b9bdce089b
forthtilliath <vincent.lisita+gh@gmail.com>
2026-09-14 14:43:41 +0200

    feat: wire new categories into ts-types root barrel

--- packages/ts-types/src/index.ts
+++ packages/ts-types/src/index.ts
@@ -1,3 +1,7 @@
 export type * from "./array/index.js";
+export type * from "./brand/index.js";
+export type * from "./common/index.js";
+export type * from "./function/index.js";
 export type * from "./helpers/index.js";
 export type * from "./object/index.js";
+export type * from "./union/index.js";
